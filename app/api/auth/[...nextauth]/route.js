da7311eb6fd23bc37d2d3f1b2cea001216b6263a
PRITAM SAHA <pritamstech@hotmail.com>
2024-10-13 20:23:20 +0530

    NextAuth Re-Defined

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,8 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import LinkdinProvider from "next-auth/providers/linkedin";
-
+import { connectDB } from "@/server/ConnectDB";
+import User from "@/models/User";
 export const authOptions = NextAuth({
   providers: [
     GithubProvider({
@@ -16,13 +17,48 @@ export const authOptions = NextAuth({
     LinkdinProvider({
       clientId: process.env.LINKEDIN_ID,
       clientSecret: process.env.LINKEDIN_SECRET,
-      scope: "openid profile email",
+      wellKnown:
+        "https://www.linkedin.com/oauth/.well-known/openid-configuration",
+      issuer: "https://www.linkedin.com/oauth",
+      authorization: {
+        params: {
+          scope: "openid profile email",
+        },
+      },
+      async profile(profile) {
+        if (profile) {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            email: profile.email,
+          };
+        }
+      },
     }),
   ],
   secret: process.env.NEXT_AUTH_SECRET,
   callbacks: {
     async session({ session, user, token }) {
-      session.user.userna;
+      let Randomstr =
+        session.user.name + Math.floor(Math.random() * 1000) + Date.now();
+      await connectDB();
+      const FindUser = await User.findOne({ email: session.user.email });
+      if (!FindUser) {
+        session.user.username = Randomstr.slice(0, -10);
+        session.user.UPI_ID = "";
+        session.user.Profilepic = "";
+        session.user.Coverpic = "";
+        await User.create({
+          name: session.user.name,
+          email: session.user.email,
+          username: session.user.username,
+        });
+        return session;
+      }
+      session.user.username = FindUser.username;
+      session.user.UPI_ID = FindUser.UPI_ID;
+      session.user.Profilepic = FindUser.Profilepic;
+      session.user.Coverpic = FindUser.Coverpic;
       return session;
     },
   },
